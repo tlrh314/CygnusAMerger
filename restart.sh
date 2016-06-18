b1a2bd1a10d02cad117ced5d2eb0b29d1dac0d8e
tlrh314 <timohalbesma@gmail.com>
2016-06-18 17:22:37 +0200

    Restarting 9978833 / 20160617T1544

--- restart.sh
+++ restart.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-#PBS -lnodes=4:ppn=16:cores16
-#PBS -lwalltime=03:00:00
+#PBS -lnodes=8:ppn=16:cores16
+#PBS -lwalltime=30:00:00
+
+send_mail() {
+    if [[ "${SYSTYPE}" == *".lisa.surfsara.nl" ]]; then
+        msg="Dear Timo,\n\n\
+I'm done with PBS JobID: ${PBS_JOBID}\n\n\
+Succesfully ran job @ ${SYSTYPE}.\n\n\
+Cheers,\n${SYSTYPE}"
+        SUBJECT="Job @ ${SYSTYPE} is done executing :-)!"
+        (echo -e $msg | mail $USER -s "${SUBJECT}") && echo "Mail Sent."
+    elif [ "${SYSTYPE}" == "taurus" ]; then
+        msg="To: timohalbesma@gmail.com\nFrom: tlrh@${SYSTYPE}\n\
+Subject: ${0} @ ${SYSTYPE} is done executing :-)!\n\n\
+Dear Timo,\n\n\
+\"${0} $@\" is now done executing.\n\n\
+Cheers,\n${SYSTYPE}"
+        (echo -e $msg | sendmail -t timohalbesma@gmail.com) && echo "Mail Sent."
+    fi
+}
 
 setup_system() {
     # Better safe than sorry *_*... TODO: turn this off for production run?
@@ -14,16 +32,29 @@ setup_system() {
         # TODO: check how multiple threas/nodes works on Lisa?
         # TODO: is the PBS situation the scheduler that also sets nodes/threads?
         # THREADS=$(grep -c ^processor /proc/cpuinfo)
-        THREADS=64
+        THREADS=128  # set based on the nr of nodes requested
         NICE=0  # default is 0
         BASEDIR="$HOME"  # TODO: look into the faster disk situation @Lisa?
+        % TO ADJUST FOR RESTARTING
+        TIMESTAMP="20160617T1544"  # qsub no parse options..
+        MAIL=true
         # TODO: I think home should not be used, instead use scratch??
         module load c/intel
         module load fftw2/sp/intel
         module load fftw2/dp/intel
         module load openmpi/intel
+
+        # Send mail once jobs starts.. it could queue god knows how long
+        msg="Dear Timo,\n\n\
+Gadget-2 run started for ${TIMESTAMP} with PBS JobID = ${PBS_JOBID}.\n\n\
+The job runs at @ ${SYSTYPE}.\n\n\
+Cheers,\n${SYSTYPE}"
+
+        SUBJECT="Job @ ${SYSTYPE} has started :-)!"
+    
+        (echo -e $msg | mail $USER -s "${SUBJECT}") 
     elif [ "${SYSTYPE}" == "taurus" ]; then
-        THREADS=8
+        THREADS=4
         NICE=19
         BASEDIR="/scratch/timo"
     elif [ "${SYSTYPE}" == "Darwin" ]; then
@@ -58,10 +89,18 @@ setup_system() {
 
 setup_system
 
-cd /scratch/timo/runs/20160526T1354/snaps
+cd ${BASEDIR}/runs/${TIMESTAMP}/snaps
 
 # If restart files are present: only change parameterfile entries with a * in users-guide
 # mpiexec Gadget2 gadget2.par 1
 
+
+# Make sure you copy restart.par to appropriate directory and adjust the
+# required settings!
+
 # If restart files are not present: restart from last snapshot and change Tbegin
-nice -n 19 mpiexec.hydra -np $THREADS ./Gadget2 restart.par
+nice -n ${NICE} mpiexec.hydra -np $THREADS ./Gadget2 restart.par
+
+if [ "$MAIL" = true ]; then
+    send_mail
+fi
