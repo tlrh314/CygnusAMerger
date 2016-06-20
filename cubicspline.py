20aaf06c0684fbfd6d2bb87c713ae77156f550aa
Timo Halbesma <timohalbesma@gmail.com>
2016-06-21 00:56:58 +0200

    Trying to poke around in kernel of Toycluster to sample with Cubic Spline

--- cubicspline.py
+++ cubicspline.py
@@ -64,7 +64,76 @@ def spline_kernel(r, h):
     elif r/h >= 1:
         return 0
 
+
+def gadget_kernel(r, h):
+    """ copy-paste from Gadget2 density.c 467 onwards """
+    KERNEL_COEFF_1 = 2.546479089470
+    KERNEL_COEFF_2 = 15.278874536822
+    KERNEL_COEFF_3 = 45.836623610466
+    KERNEL_COEFF_4 = 30.557749073644
+    KERNEL_COEFF_5 = 5.092958178941
+    KERNEL_COEFF_6 = (-15.278874536822)
+    h2 = h*h
+    hinv = 1.0/h
+    hinv3 = hinv*hinv*hinv
+    hinv4 = hinv3 * hinv
+    u = r * hinv
+    if (u > 1):
+        wk = 0
+    elif(u < 0.5):
+        wk = hinv3 * (KERNEL_COEFF_1 + KERNEL_COEFF_2 * (u - 1) * u * u)
+        # dwk = hinv4 * u * (KERNEL_COEFF_3 * u - KERNEL_COEFF_4)
+    else:
+        wk = hinv3 * KERNEL_COEFF_5 * (1.0 - u) * (1.0 - u) * (1.0 - u)
+        # dwk = hinv4 * KERNEL_COEFF_6 * (1.0 - u) * (1.0 - u)
+    return wk
+
+
+
+def kernel_spline_3d(r, h):
+    """ Copy-paste from P-Smac2 project_sph.c line 336 """
+    r /= h
+
+    u = 1 - r
+
+    if (r > 1):
+        return 0
+    elif (r > 0.5):
+        return 8 / numpy.pi * 2 * u * u * u
+    else:
+        return 8 / numpy.pi * (1 - 6 * r * r * u)
+
+
+def psmac2_kernel_vs_gadget_kernel():
+    from numpy import vectorize
+    vkernel_spline_3d = vectorize(kernel_spline_3d)
+    epsilon = 1
+    h = 2.8 * epsilon
+    r = numpy.arange(0, 2*h, 0.001)
+    psmac2 = vkernel_spline_3d(r, h)
+    psmac2_hank = numpy.zeros(len(r))
+    gadget = numpy.zeros(len(r))
+    price = numpy.zeros(len(r))
+    for i, hank in enumerate(r):
+        # all three differ, yay
+        psmac2_hank[i] = kernel_spline_3d(hank, h)
+        gadget[i] = gadget_kernel(hank, h)
+        price[i] = spline_kernel(hank, h)
+
+    pyplot.figure(figsize=(12, 9))
+    pyplot.plot(r, psmac2, label="psmac")
+    pyplot.plot(r, gadget, label="gadget")
+    pyplot.plot(r, price, label="price")
+    # pyplot.plot(r, psmac2_hank)
+    pyplot.legend()
+    pyplot.show()
+
+
 if __name__ == "__main__":
+    psmac2_kernel_vs_gadget_kernel()
+    import sys; sys.exit(0)
+
+
     r = numpy.arange(0, 2.8, 0.001)
     W_of_r_cubic = numpy.zeros(len(r))
     W_of_r_quartic = numpy.zeros(len(r))
