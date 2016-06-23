bb96890cf52b1564d9d5d111a65510b2119104c5
Timo Halbesma <timohalbesma@gmail.com>
2016-06-23 18:26:54 +0200

    Julius' ffmpeg parameters

--- make_movie.sh
+++ make_movie.sh
@@ -10,7 +10,11 @@ cd "../runs/${TIMESTAMP}/out"
 # -r 5                  framerate
 # -i snapshot_%3d.png   name of input files /w 3 digits (e.g. 000, 001, 002)
 
-nice -n 19 ffmpeg -y -r 5 -i snapshot_projection-${PROJECTION}_%3d.png "${TIMESTAMP}_${PROJECTION}.mp4"
+FPS=25
+
+nice -n 19 ffmpeg -y -r $FPS -i snapshot_projection-${PROJECTION}_%3d.png \
+    -profile:v high444 -level 4.1 -c:v libx264 -preset slow -crf 25 -s '2000:2000' \
+    -an "${TIMESTAMP}_${PROJECTION}.mp4"
 
 # Should work for Telegram.. but only works on MBP, not iOS
 # ffmpeg -y -r 5 -i snapshot_%3d.png -acodec libfaac -b:a 128k -vcodec mpeg4 -b:v 1200k -flags +aic+mv4 movie.mp4 
