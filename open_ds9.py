e5dda3e348c5a2029bf70019ceb0ea8086ebba4f
Timo Halbesma <timohalbesma@gmail.com>
2016-08-31 17:40:15 +0200

    Added observation fits file

--- open_ds9.py
+++ open_ds9.py
@@ -7,13 +7,13 @@ import logging
 try:
     import pyds9
 except ImportError:
-    print('Program requires installation of ds9')
+    print("Program requires installation of ds9")
     exit(1)
 
 try:
     import IPython
 except ImportError:
-    print('Program requires IPython installation')
+    print("Program requires IPython installation")
     exit(1)
 try:
     from astropy.io import fits as pyfits
@@ -30,19 +30,19 @@ else:
 
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s|%(name)s|%(levelname)s|%(message)s')
-logger = logging.getLogger('open_ds9')
+                    format="%(asctime)s|%(name)s|%(levelname)s|%(message)s")
+logger = logging.getLogger("open_ds9")
 
 
 def open_file(d, fname):
     try:
-        d.set('file {}'.format(fname))
+        d.set("file {}".format(fname))
     except ValueError as err:
         print str(err)
         print has_fits
-        if 'XPA$ERROR Unable to load fits' in str(err) and has_fits:
+        if "XPA$ERROR Unable to load fits" in str(err) and has_fits:
             logger.exception(
-                'Cannot load fits file natively, using pyfits')
+                "Cannot load fits file natively, using pyfits")
             d.set_np2arr(pyfits.getdata(fname).astype(float))
         else:
             raise
@@ -67,19 +67,26 @@ def main(args):
     #                 print "    Invalid character"
     # else:
     # List of commands: http://ds9.si.edu/doc/ref/command.html
+
+    if args.chandra:
+        rundir = "/Volumes/SURFlisa/runs/"
+        mosaic = "ChandraObservation/StruisMosaics/mosaic/cygnus_tot_flux.fits"
+        args.filename = rundir+mosaic
+
+
     d = pyds9.DS9()
     time.sleep(0.5)
     open_file(d, args.filename)
 
-    d.set('preserve pan yes')
-    d.set('preserve scale yes')
+    d.set("preserve pan yes")
+    d.set("preserve scale yes")
     print d.id
     print d.target
 
-    header = 'DS9 instance in object `d`'
+    header = "DS9 instance in object `d`"
     if has_fits:
         f = pyfits.open(args.filename)
-        header += '\npyfits instance in object `f`'
+        header += "\npyfits instance in object `f`"
 
     if "temperature" in args.filename:
         d.set("scale linear")
@@ -108,11 +115,26 @@ def main(args):
     # d.set("scale scope global")  # takes forever
     # d.set("scale open")
 
-    # seems slow until al frames have been loaded at least once
-    d.set("cube interval 0.125")
-    d.set("cube play")
-    #d.set("cube close")
-    # d.set("cube 35")
+    if "fits.fz" in args.filename:
+        # Simulation
+        # seems slow until al frames have been loaded at least once
+        d.set("cube interval 0.125")
+        d.set("cube play")
+        #d.set("cube close")
+        # d.set("cube 35")
+
+    if "cygnus_tot_flux.fits" in args.filename:
+        # Observation
+        d.set("scale log")
+        d.set("scale limits 7.0e-10 1.0e-6")
+        d.set("cmap sls")
+        d.set("smooth radius 9")
+        d.set("smooth yes")
+        d.set("zoom to fit")
+        # contrast and bias
+        d.set("cmap value 3.08889 0.30112")
+        # default
+        # d.set("cmap value 1 0.5")
 
     # reg = "/Users/timohalbesma/Desktop/TemperatureJump/Simulation/"+\
     #    "20160819T2357.reg"
@@ -127,15 +149,17 @@ def main(args):
     #pyplot.imshow(fits[0], origin="lower")
     #pyplot.show()
 
-    IPython.embed(banner1='', header=header)
+    IPython.embed(banner1="", header=header)
 
     # Close the ds9 window if it is still open
     if pyds9.ds9_targets() and "DS9:{0} {1}".format(d.target, d.id) in pyds9.ds9_targets():
         d.set("exit")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
-    parser.add_argument('--zscale', action='store_true')
+    parser.add_argument("filename")
+    parser.add_argument("--zscale", action="store_true")
+    parser.add_argument("--chandra", action="store_true")
+
     main(parser.parse_args())
